0e0bedec06071d71fc65e998393eb8172e3db44c
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-14 20:24:36 +0530

    completed

--- src/Component/LaunchView/LaunchView.jsx
+++ src/Component/LaunchView/LaunchView.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import axios from "axios";
 import { withRouter } from "react-router";
+import { Zoom } from "react-slideshow-image";
 import "./styles.css";
+
+const zoomOutProperties = {
+  duration: 1000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  scale: 0.4,
+  arrows: true,
+};
 
 export class LaunchView extends React.Component {
   state = {
-    launch: {},
+    launch: {
+      links: {
+        flickr_images: [],
+      },
+    },
   };
   componentDidMount = () => {
     const flightNo = this.props.match.params.flight_number;
@@ -35,12 +49,18 @@ export class LaunchView extends React.Component {
   };
 
   render() {
-    console.log(this.state);
+    const hashImages = this.state.launch.links.flickr_images.length > 0;
     return (
       <div className="launch-view">
         {this.launchAttribute("Mission Name", "mission_name")}
         {this.launchAttribute("Flight Number", "flight_number")}
         {this.launchAttribute("Launch Date", "launch_date_local")}
+        <div className="flickr_images">
+          {hashImages &&
+            this.state.launch.links.flickr_images.map((each, index) => (
+              <img key={index} style={{ width: "100%" }} src={each} />
+            ))}
+        </div>
         <p className="description">{this.state.launch.details}</p>
       </div>
     );
