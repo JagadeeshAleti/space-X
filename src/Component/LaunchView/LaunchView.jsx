928e63572e8dc7516750b994e220cae6a528f45c
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-14 10:36:07 +0530

    get single launch info statisclyy

--- src/Component/LaunchView/LaunchView.jsx
+++ src/Component/LaunchView/LaunchView.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import axios from "axios";
 export class LaunchView extends React.Component {
+  state = {
+    launch: {},
+  };
+  componentDidMount = () => {
+    this.getLaunchView();
+  };
+  getLaunchView = () => {
+    axios
+      .get("https://api.spacexdata.com/v3/launches/2")
+      .then((response) => {
+        this.setState({
+          launch: response.data,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1>Launches</h1>
